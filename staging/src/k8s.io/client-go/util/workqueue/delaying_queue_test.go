dd30f6afbd9f98360a3289cacb5e11bc485454c8
Ying Huang <sindica2000@yahoo.com>
2021-07-08 00:07:39 +0000

    PR 82365 commit 1: pkg/util/workqueue/delaying_queue: export contructor with custom clock jkaniuk @2019-10-23

--- staging/src/k8s.io/client-go/util/workqueue/delaying_queue_test.go
+++ staging/src/k8s.io/client-go/util/workqueue/delaying_queue_test.go
@@ -29,7 +29,7 @@ import (
 
 func TestSimpleQueue(t *testing.T) {
 	fakeClock := clock.NewFakeClock(time.Now())
-	q := newDelayingQueue(fakeClock, "")
+	q := NewDelayingQueueWithCustomClock(fakeClock, "")
 
 	first := "foo"
 
@@ -71,7 +71,7 @@ func TestSimpleQueue(t *testing.T) {
 
 func TestDeduping(t *testing.T) {
 	fakeClock := clock.NewFakeClock(time.Now())
-	q := newDelayingQueue(fakeClock, "")
+	q := NewDelayingQueueWithCustomClock(fakeClock, "")
 
 	first := "foo"
 
@@ -130,7 +130,7 @@ func TestDeduping(t *testing.T) {
 
 func TestAddTwoFireEarly(t *testing.T) {
 	fakeClock := clock.NewFakeClock(time.Now())
-	q := newDelayingQueue(fakeClock, "")
+	q := NewDelayingQueueWithCustomClock(fakeClock, "")
 
 	first := "foo"
 	second := "bar"
@@ -179,7 +179,7 @@ func TestAddTwoFireEarly(t *testing.T) {
 
 func TestCopyShifting(t *testing.T) {
 	fakeClock := clock.NewFakeClock(time.Now())
-	q := newDelayingQueue(fakeClock, "")
+	q := NewDelayingQueueWithCustomClock(fakeClock, "")
 
 	first := "foo"
 	second := "bar"
@@ -219,7 +219,7 @@ func BenchmarkDelayingQueue_AddAfter(b *testing.B) {
 	r := rand.New(rand.NewSource(time.Now().Unix()))
 
 	fakeClock := clock.NewFakeClock(time.Now())
-	q := newDelayingQueue(fakeClock, "")
+	q := NewDelayingQueueWithCustomClock(fakeClock, "")
 
 	// Add items
 	for n := 0; n < b.N; n++ {
